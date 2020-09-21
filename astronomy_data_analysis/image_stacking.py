ff19bbcfd4b605a54535a5f3a2ab64892e9a636a
VeggieBaconator <julieannabacon2014@gmail.com>
2020-09-21 13:56:38 -0400

    Finished implementing binapprox algorithm, fixed test file paths to default to included sample data. Added more sample data.

--- astronomy_data_analysis/image_stacking.py
+++ astronomy_data_analysis/image_stacking.py
@@ -4,35 +4,18 @@ Here we only have functions used for image stacking.
 import numpy as np
 from astropy.io import fits
 from . import fits_tools
+import matplotlib.pyplot as plt
 
 def mean_fits(data_stack):
     """
-    Takes in list of fits files
-    Each fits file must have image of the same shape
-    Returns the mean value across the same element in each fits files
-    shape of return is same as shape of each image file
+    Returns the mean value across the same element in each fits files.
+    Return has the same shape as one image in data_stack.
 
     Parameters
     ----------
     data_stack: 3D numpy array of fits image data. See fits_tools.get_data_stack.
     """
-    return np.mean(data_stack, axis = 0)
-
-
-def brightest_pixel(image):
-    """
-    Takes data stack of fits image data
-    Returns the pixel (array like) value where the image is brightest
-
-    Parameters
-    ----------
-    image: 2D numpy array or 3D numpy array of image data. See fits_tools.get_data_stack.
-    """
-    # find the pixel with the brightest value in the image
-    unraveled_location = np.argmax(np.array(image)) # argmax returns index in flattened version of array
-    location = np.unravel_index(unraveled_location, np.shape(image)) # get back our location in our 2D image
-
-    return location
+    return np.mean(data_stack, axis=0)
 
 
 def scalable_mean_fits(data_stack):
@@ -50,3 +33,96 @@ def scalable_mean_fits(data_stack):
     mean = hist / len(data_stack)
     return mean
 
+
+def median_fits(data_stack):
+    """
+    Returns the median value across the same element in each fits files.
+    Return has the same shape as one image in data_stack.
+
+    Parameters
+    ----------
+    data_stack: 3D numpy array of fits image data. See fits_tools.get_data_stack.
+    """
+    return np.median(data_stack, axis=0)
+
+
+def scalable_median_fits(data_stack, num_bins=5):
+    """
+    Implements numpythonic binapprox algorithm
+    For more details: http://www.stat.cmu.edu/~ryantibs/median/
+
+    TODO fix the data stack stuff, use scalable mean and std for computing stats in hist
+
+    Parameters
+    ----------
+    file_list: blad
+    num_bins: int
+
+    """
+    mean, std, left_bins, bins = median_histogram(data_stack, num_bins)
+    mid = (len(data_stack) + 1) / 2 # we only want to count until we've reached the median value
+
+    bin_width = 2 * std / num_bins
+
+    cumsumm = np.cumsum(np.dstack((left_bins, bins)), axis=2)
+    b = np.argmax(cumsumm >= mid, axis=2) - 1
+
+    median = mean - std + bin_width*(b + 0.5)
+    return median
+
+
+def median_histogram(data_stack, num_bins=5):
+    """
+    Constructs the histogram for scalable_median_fits. End result will have a histogram for
+    each "pixel" of the fits image.
+
+    Parameters
+    ----------
+    file_list: blad
+    num_bins: int
+
+
+    Returns
+    -------
+    mean:   2D numpy array with dimensions = input image. Mean image of all of the data.
+    std:    2D numpy array with dimensions = input image. Std of all of the data.
+    left_bins: 2D numpy array with dimensions = input image. Number of pixels who were more than 1 std from mean.
+    bins:   3D numpy array with dimensions = (input image shape, num_bins). Histogram.
+    """
+    mean = mean_fits(data_stack)
+    std = np.std(data_stack, axis=0)
+
+    minval = mean - std
+    maxval = mean + std
+    bin_width = 2 * std / num_bins
+
+    bins = np.zeros((data_stack[0].shape[0], data_stack[0].shape[1], num_bins)) # , dtype=int TODO think about this
+    # we are going to count the number of times we see a value less than mean - std.
+    left_bins = np.zeros(data_stack[0].shape)
+
+    for i, data in enumerate(data_stack):
+        low_values = (data < minval)
+        left_bins[low_values] +=1
+
+        in_range = (data >= minval) & (data < maxval)
+        bin_val = (data - minval) / bin_width
+
+        bin_index = np.array((data[in_range] - (minval[in_range])) / bin_width[in_range], dtype=int)
+        bins[in_range, bin_index] += 1
+    return mean, std, left_bins, bins
+
+
+def brightest_pixel(image):
+    """
+    Takes data stack of fits image data.
+    Returns the pixel (array like) value where the image is brightest.
+
+    Parameters
+    ----------
+    image: 2D numpy array or 3D numpy array of image data. See fits_tools.get_data_stack.
+    """
+    # find the pixel with the brightest value in the image
+    unraveled_location = np.argmax(np.array(image)) # argmax returns index in flattened version of array
+    location = np.unravel_index(unraveled_location, np.shape(image)) # get back our location in our 2D image
+
+    return location
--- test.py
+++ test.py
@@ -11,15 +11,17 @@ import astronomy_data_analysis as ada
 
 # user can provide the path to fits files via the command line or by changing the variable path below
 if len(sys.argv) > 1:
-    path = sys.argv[1]
+    parent_directory = sys.argv[1]
 else:
-    path = '/home/bacon/code/datasets/astronomy_data/pulsar images/'
+    # NOTE: Only need to change this in order to run test.py
+    parent_directory = '/home/bacon/code/python_toys/'
 
 
 # our list of fits file paths of our pulsar images
+path = parent_directory + 'astronomy_data_analysis/sample_data/pulsar_images/'
 file_list = [path + '000{0}.fits'.format(i) for i in range(0, 9)]
 image_list = ada.fits_tools.get_data_stack(file_list)
-ada.fits_tools.plot_images(image_list, title="Pulsar Images")
+ada.fits_tools.plot_images(image_list, title="Pulsar Images", ncols=3)
 
 
 # demonstrating that the space efficient mean (scalable mean) and mean produce identical results
@@ -27,6 +29,15 @@ space_mean = ada.image_stacking.scalable_mean_fits(image_list)
 mean = ada.image_stacking.mean_fits(image_list)
 assert np.all(np.isclose(space_mean, mean))
 
+
+# demonstrating the space efficient median and median
+path = parent_directory + 'astronomy_data_analysis/sample_data/median_images/'
+file_list = [path + 'image{0}.fits'.format(i) for i in range(0, 5)]
+image_list = ada.fits_tools.get_data_stack(file_list)
+
+median = ada.image_stacking.median_fits(image_list)
+space_median = ada.image_stacking.scalable_median_fits(image_list, num_bins=100)
+quit()
 
 # demonstrating another function from image_stacking
 locations = []
@@ -37,33 +48,34 @@ print("Locations of the brightest pixels: ", locations)
 
 
 # some examples from naive_cross_matching
-path = '/home/bacon/code/datasets/astronomy_data/star_catalogs/'
+print("Starting crossmatching examples...")
+path = parent_directory + 'astronomy_data_analysis/sample_data/star_catalogs/'
 bss_cat = ada.cross_matching_tools.load_bss(path)
 cosmos_cat = ada.cross_matching_tools.load_cosmos(path)
 
 
 # First example
-max_dist = np.radians(40/3600)
-matches, no_matches = ada.naive_cross_matching.naive_crossmatch(bss_cat, cosmos_cat, max_dist)
-print(matches[:3])
-print(no_matches[:3])
-
-
-# Second example
 start = time.time()
 max_dist = np.radians(5/3600)
 matches, no_matches = ada.naive_cross_matching.naive_crossmatch(bss_cat, cosmos_cat, max_dist)
 elapsed = time.time() - start
-print("this is the time taken by naive crossmatch ", elapsed)
+print("This is the time taken by naive crossmatch ", elapsed)
+print("Number of matches with small search radius: ", len(matches))
+print("Number of unmatched objects with small search radius: ", len(no_matches))
+
+
+# Second example
+max_dist = np.radians(40/3600)
+matches, no_matches = ada.naive_cross_matching.naive_crossmatch(bss_cat, cosmos_cat, max_dist)
+print("Number of matches with large search radius: ", len(matches))
+print("Number of unmatched objects with large search radius: ", len(no_matches))
 
 
 # First example, using k-d implementation
 start = time.time()
-matches, no_matches = ada.kd_cross_matching.crossmatch(bss_cat, cosmos_cat, max_dist=np.radians(40/3600))
+kd_matches, kd_no_matches = ada.kd_cross_matching.crossmatch(bss_cat, cosmos_cat, max_dist=np.radians(max_dist))
 elapsed = time.time() - start
-print(matches[:3])
-print(no_matches[:3])
-print("this is the time taken by kd crossmatch ", elapsed)
+print("This is the time taken by kd crossmatch ", elapsed)
 
 
 # TODO
